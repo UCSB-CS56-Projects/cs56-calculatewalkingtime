01ff6a1a30e93f2d6bcd1795a4786f29d5fa7cdf
Charles Gu <cgu@umail.ucsb.edu>
2017-11-12 23:45:27 -0800

    fixed issue #13

--- src/MapGui.java
+++ src/MapGui.java
@@ -60,7 +60,7 @@ public class MapGui extends JFrame
 	
 	BuildingCheckBox LIB = new BuildingCheckBox();
 	LIB.Name = "LIB";
-	LIB.setBounds(460, 430, 20,20);
+	LIB.setBounds(460,430,20,20);
 	transparentPanel.add(LIB);
 	boxes.add(LIB);
 
@@ -129,7 +129,7 @@ public class MapGui extends JFrame
 	boxes.add(HSSB);
 	transparentPanel.add(HSSB);
 
-	//add MANZI
+	//add MANZI 110
 
 	BuildingCheckBox MANZI = new BuildingCheckBox();
 	MANZI.Name = "MANZ";
@@ -163,7 +163,6 @@ public class MapGui extends JFrame
 	transparentPanel.add(NH);
 
 
-
 	//add GIRVETZ 
 
 	BuildingCheckBox GIRV = new BuildingCheckBox();
@@ -186,52 +185,123 @@ public class MapGui extends JFrame
 	BuildingCheckBox ARTS = new BuildingCheckBox();
 	ARTS.Name = "ARTS";
 	ARTS.setBounds(246,500,20,20);
-	transparentPanel.add(ARTS); 
 	boxes.add(ARTS);
+	transparentPanel.add(ARTS);
 
 	//add BIO LAB 2
 
 	BuildingCheckBox BIOL2 = new BuildingCheckBox();
 	BIOL2.Name = "BIOL2";
 	BIOL2.setBounds(633,520,20,20);
-	transparentPanel.add(BIOL2); 
 	boxes.add(BIOL2);
+	transparentPanel.add(BIOL2); 
 
 	//add BROIDA
+
+	BuildingCheckBox BROIDA = new BuildingCheckBox();
+	BROIDA.Name = "BROIDA";
+	BROIDA.setBounds(595,380,20,20);
+	transparentPanel.add(BROIDA);
+	boxes.add(BROIDA);
 
 	//add BREN
 
 	BuildingCheckBox BREN = new BuildingCheckBox();
 	BREN.Name = "BREN";
-	BREN.setBounds(653,500,20,20);
-	transparentPanel.add(BREN); 
+	BREN.setBounds(653,500,20,20); 
 	boxes.add(BREN);
+	transparentPanel.add(BREN); 
 
 	//add BUCHN HALL
-
+	BuildingCheckBox BUCHN_HALL = new BuildingCheckBox();
+	BUCHN_HALL.Name = "BUCHN HALL";
+	BUCHN_HALL.setBounds(520,316,20,20);
+	boxes.add(BUCHN_HALL);
+	transparentPanel.add(BUCHN_HALL);
+			    
 	//add CHEM
+	BuildingCheckBox CHEM = new BuildingCheckBox();
+	CHEM.Name = "CHEM";
+	CHEM.setBounds(660,306,20,20);
+	boxes.add(CHEM);
+	transparentPanel.add(CHEM);
 
 	//add ENGR2
+	BuildingCheckBox ENGR2 = new BuildingCheckBox();
+	ENGR2.Name = "ENGR2";
+	ENGR2.setBounds(420,420,20,20);
+	transparentPanel.add(ENGR2);
+	boxes.add(ENGR2);
 
 	//add KOHN
+	BuildingCheckBox KOHN = new BuildingCheckBox();
+	KOHN.Name = "KOHN";
+	KOHN.setBounds(800,403,20,20);
+	boxes.add(KOHN);
+	transparentPanel.add(KOHN);
 
 	//add LSB
+	BuildingCheckBox LSB = new BuildingCheckBox();
+	LSB.Name = "LSB";
+	LSB.setBounds(585,570,20,20);
+	boxes.add(LSB);
+	transparentPanel.add(LSB);
 
 	//add MUSIC
+	BuildingCheckBox MUSIC = new BuildingCheckBox();
+	MUSIC.Name = "MUSIC";
+	MUSIC.setBounds(370,517,20,20);
+	boxes.add(MUSIC);
+	transparentPanel.add(MUSIC);
 
 	//add NOBLE
+	BuildingCheckBox NOBLE = new BuildingCheckBox();
+	NOBLE.Name = "Noble";
+	NOBLE.setBounds(550,510,20,20);
+	boxes.add(NOBLE);
+	transparentPanel.add(NOBLE);
 
 	//add SAN MIGEL
+	BuildingCheckBox SAN_MIGEL = new BuildingCheckBox();
+	SAN_MIGEL.Name = "SAN MIGEL";
+	SAN_MIGEL.setBounds(370,645,20,20);
+	transparentPanel.add(SAN_MIGEL);
+	boxes.add(SAN_MIGEL);
 
 	//add SAN RAFEL
-
+	BuildingCheckBox SAN_RAFEL = new BuildingCheckBox();
+	SAN_RAFEL.Name = "SAN RAFEL";
+	SAN_RAFEL.setBounds(110,610,20,20);
+	boxes.add(SAN_RAFEL);
+	transparentPanel.add(SAN_RAFEL);
+	
 	//add SANTA CRUZ
+	BuildingCheckBox SANTA_CRUZ = new BuildingCheckBox();
+	SANTA_CRUZ.Name = "SANTA CRUZ";
+	SANTA_CRUZ.setBounds(590,680,20,20);
+	boxes.add(SANTA_CRUZ);
+	transparentPanel.add(SANTA_CRUZ);
 
 	//add SANTA ROSA
-
+	BuildingCheckBox SANTA_ROSA = new BuildingCheckBox();
+	SANTA_ROSA.Name = "SANTA ROSA";
+	SANTA_ROSA.setBounds(500,610,20,20);
+	boxes.add(SANTA_ROSA);
+	transparentPanel.add(SANTA_ROSA);
+	
 	//add WEBB
+	BuildingCheckBox WEBB = new BuildingCheckBox();
+	WEBB.Name = "WEBB";
+	WEBB.setBounds(585,459,20,20);
+	boxes.add(WEBB);
+	transparentPanel.add(WEBB);
 
 	//add DLG
+	BuildingCheckBox DLG = new BuildingCheckBox();
+	DLG.Name = "DLG";
+	DLG.setBounds(470,605,20,20);
+	boxes.add(DLG);
+	transparentPanel.add(DLG);
 
 	//****END OF BUILDINGS*****
 
